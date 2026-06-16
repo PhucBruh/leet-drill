f21acdd1870ca006ae717705bb3d1f98dd4e7757
PhucBruh <phucdev000@gmail.com>
2026-06-16 22:10:42 +0700

    greedy choice for 21

---
+++ leetcode/21-merge-two-sorted-lists.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+# greedy choice approach
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode(-1)
+        head = dummy
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                dummy.next = list1
+                list1 = list1.next
+            else:
+                dummy.next = list2
+                list2 = list2.next
+
+            dummy = dummy.next
+
+        dummy.next = list1 or list2
+        return head.next
+
+
+l1 = ListNode(1, ListNode(2, ListNode(4)))
+l2 = ListNode(1, ListNode(3, ListNode(4)))
+solution = Solution()
+result = solution.mergeTwoLists(l1, l2)
+print(result.val)
